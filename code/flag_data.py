8c0269f5293efa24c289d457a522e1fd4a9383cd
apopping <a.popping@gmail.com>
2019-01-22 15:29:35 +0800

    different flagval

--- code/flag_data.py
+++ code/flag_data.py
@@ -24,7 +24,7 @@ def flag_data(file):
     print(f"flagging : {file}")
     os.system('uvflag vis=' + file + ' edge=20 flagval=flag')
     os.system('uvflag vis=' + file + ' select="shadow(25)" flagval=flag')
-    os.system('tvclip vis=' + file + ' clip=20 options=notv commands=diff,clip')
+    os.system('tvclip vis=' + file + ' clip=5 options=notv commands=diff,clip')
     os.system('pgflag vis=' + file + ' stokes=ii command="<be" flagpar="5,5,3,3,5,3,20" options=nodisp')
 
 
