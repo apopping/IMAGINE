0d47d0d3948a6eeecb86ed673599f160807f1dae
apopping <a.popping@gmail.com>
2019-02-09 00:41:46 +0800

    update run all script

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,1 +1,4 @@
-python combine_galaxy.py -g ngc3137 -dd /mnt/science1/imagine/rawdata/ngc3137/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc2997 -dd /mnt/science1/imagine/rawdata/ngc2997/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g eso274-001 -dd /mnt/science1/imagine/rawdata/eso274-001/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g eso209-009 -dd /mnt/science1/imagine/rawdata/eso209-009/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ic5201 -dd /mnt/science1/imagine/rawdata/ic5201/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
