f3360ea6a3a053607625d8067a0f1059b8bca52a
apopping <a.popping@gmail.com>
2019-02-09 23:11:42 +0800

    updated db

--- code/run_all.sh
+++ code/run_all.sh
@@ -2,3 +2,33 @@ python combine_galaxy.py -g ngc2997 -dd /mnt/science1/imagine/rawdata/ngc2997/ -
 python combine_galaxy.py -g eso274-001 -dd /mnt/science1/imagine/rawdata/eso274-001/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
 python combine_galaxy.py -g eso209-009 -dd /mnt/science1/imagine/rawdata/eso209-009/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
 python combine_galaxy.py -g ic5201 -dd /mnt/science1/imagine/rawdata/ic5201/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+
+python combine_galaxy.py -g ngc3137 -dd /mnt/science1/imagine/rawdata/ngc3137/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g eso138-010 -dd /mnt/science1/imagine/rawdata/eso138-010/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g eso154-023 -dd /mnt/science1/imagine/rawdata/eso154-023/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g eso214-017 -dd /mnt/science1/imagine/rawdata/eso214-017/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g eso270-017 -dd /mnt/science1/imagine/rawdata/eso270-017/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ic5052 -dd /mnt/science1/imagine/rawdata/ic5052/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc1433 -dd /mnt/science1/imagine/rawdata/ngc1433/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc1512 -dd /mnt/science1/imagine/rawdata/ngc1512/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc1515 -dd /mnt/science1/imagine/rawdata/ngc1515/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc1617 -dd /mnt/science1/imagine/rawdata/ngc1617/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc1744 -dd /mnt/science1/imagine/rawdata/ngc1744/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc1792 -dd /mnt/science1/imagine/rawdata/ngc1792/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc2188 -dd /mnt/science1/imagine/rawdata/ngc2188/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc24 -dd /mnt/science1/imagine/rawdata/ngc24/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc2835 -dd /mnt/science1/imagine/rawdata/ngc2835/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc2997 -dd /mnt/science1/imagine/rawdata/ngc2997/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc3109 -dd /mnt/science1/imagine/rawdata/ngc3139/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc3175 -dd /mnt/science1/imagine/rawdata/ngc3175/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc3511 -dd /mnt/science1/imagine/rawdata/ngc3511/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc45 -dd /mnt/science1/imagine/rawdata/ngc45/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc625 -dd /mnt/science1/imagine/rawdata/ngc625/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc6300 -dd /mnt/science1/imagine/rawdata/ngc6300/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc7090 -dd /mnt/science1/imagine/rawdata/ngc7090/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc7424 -dd /mnt/science1/imagine/rawdata/ngc7424/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+python combine_galaxy.py -g ngc7793 -dd /mnt/science1/imagine/rawdata/ngc7793/ -od /mnt/science1/imagine/test_processed/ -r yes -p /home/apopping/anaconda3/bin/python -o yes
+
+
+
+
